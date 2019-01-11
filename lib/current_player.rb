b6afb37f2b4534f5cde1b10fc46d0120480176f8
Abhijit Roy <aroy30@uic.edu>
2019-01-11 12:58:09 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,15 +10,16 @@ end
 
 
 def current_player(board)
+  # --- conditional trick 1 ---
   # if turn_count(board) % 2 == 0
   #   "X"
   # else
   #   "O"
   # end
-  if turn_count(board).even?
-    "X"
-  else
-    "O"
-  end
+  # if turn_count(board).even?
+  #   "X"
+  # else
+  #   "O"
+  # end
 
 end
